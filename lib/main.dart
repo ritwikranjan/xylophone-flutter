f824714f99e0d2fec1815884ea4ab715089205b0
ritwikranjan <ritwikranjan99@gmail.com>
2020-07-29 18:40:19 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +9,62 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              XylophoneSoundBar(
+                colorBar: Colors.purple,
+                audio: 'note1.wav',
+              ),
+              XylophoneSoundBar(
+                colorBar: Colors.indigo,
+                audio: 'note2.wav',
+              ),
+              XylophoneSoundBar(
+                colorBar: Colors.blue,
+                audio: 'note3.wav',
+              ),
+              XylophoneSoundBar(
+                colorBar: Colors.green,
+                audio: 'note4.wav',
+              ),
+              XylophoneSoundBar(
+                colorBar: Colors.yellow,
+                audio: 'note5.wav',
+              ),
+              XylophoneSoundBar(
+                colorBar: Colors.orange,
+                audio: 'note6.wav',
+              ),
+              XylophoneSoundBar(
+                colorBar: Colors.red,
+                audio: 'note7.wav',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class XylophoneSoundBar extends StatelessWidget {
+  final Color colorBar;
+  final String audio;
+
+  const XylophoneSoundBar({Key key, this.colorBar, this.audio})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        color: colorBar,
+        onPressed: () {
+          final player = AudioCache();
+          player.play(audio);
+        },
+        child: Container(
+          width: double.infinity,
         ),
       ),
     );
